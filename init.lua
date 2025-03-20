f293104faf7c636050640cfe7818265389fe869e
n0tlu5 <darfi.sultoni@gdplabs.id>
2025-03-21 06:17:46 +0700

    feat: add taskwarrior; zk use telescope; add zk keymaps

--- init.lua
+++ init.lua
@@ -204,10 +204,12 @@ vim.api.nvim_set_keymap('n', '<leader>zo', "<Cmd>ZkNotes { sort = { 'modified' }
 vim.api.nvim_set_keymap('n', '<leader>zt', '<Cmd>ZkTags<CR>', opts)
 
 -- Search for the notes matching a given query.
-vim.api.nvim_set_keymap('n', '<leader>zf', "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", opts)
+vim.api.nvim_set_keymap('n', '<leader>zf', "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
+vim.api.nvim_set_keymap('n', '<leader>zs', "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", opts)
 -- Search for the notes matching the current visual selection.
 vim.api.nvim_set_keymap('v', '<leader>zf', ":'<,'>ZkMatch<CR>", opts)
 vim.api.nvim_set_keymap('n', '<leader>zi', '<Cmd>ZkIndex<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>zd', '<Cmd>ZkNew { dir="journal/daily" }<CR>', opts)
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -943,7 +945,31 @@ require('lazy').setup({
     config = function()
       require('zk').setup {
         -- See Setup section below
+        picker = 'telescope',
       }
+    end,
+  },
+  {
+    'huantrinh1802/m_taskwarrior_d.nvim',
+    version = '*',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    config = function()
+      -- Require
+      require('m_taskwarrior_d').setup()
+      -- Optional
+      vim.api.nvim_set_keymap('n', '<leader>te', '<cmd>TWEditTask<cr>', { desc = 'TaskWarrior Edit', noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>tv', '<cmd>TWView<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>tu', '<cmd>TWUpdateCurrent<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>ts', '<cmd>TWSyncTasks<cr>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<c-space>', '<cmd>TWToggle<cr>', { silent = true })
+      -- Be caution: it may be slow to open large files, because it scan the whole buffer
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost' }, {
+        group = vim.api.nvim_create_augroup('TWTask', { clear = true }),
+        pattern = '*.md,*.markdown', -- Pattern to match Markdown files
+        callback = function()
+          vim.cmd 'TWSyncTasks'
+        end,
+      })
     end,
   },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
