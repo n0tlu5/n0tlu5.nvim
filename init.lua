f58519aae311a20381043655e2ecd18392802fd6
darfisultoni <darfi.sultoni@gdplabs.id>
2025-05-21 08:28:44 +0700

    feat: use catppuccin mocha

--- init.lua
+++ init.lua
@@ -899,24 +899,15 @@ require('lazy').setup({
     end,
   },
   {
-    'folke/tokyonight.nvim',
-    name = 'tokyonight',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     opts = {
-      transparent = true,
-      styles = {
-        sidebars = 'transparent',
-        floats = 'transparent',
-      },
+      flavour = 'mocha',
+      transparent_background = true, -- disables setting the background color.
     },
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-moon'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
   -- Highlight todo, notes, etc in comments
